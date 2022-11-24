a57e11993526a9f7c91ba55786721d3e406ce565
Vikrame Vasudev <vikrame1999@gmail.com>
2022-11-23 19:52:25 -0800

    Basic encoder decoder implementation with SGD

---
+++ Encoder_Decoder/train_custom_model.py
@@ -0,0 +1,206 @@
+from tqdm import tqdm
+from io import open
+import pandas as pd
+import unicodedata
+import string
+import re
+import random
+import time
+import torch
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+
+from utils import Lang, read_dataset, SOS_token, EOS_token, SEP_token, timeSince, tensorsFromPair
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+DATASET_PATH = "datasets/merged_dataset.csv"
+MAX_LENGTH = 50
+teacher_forcing_ratio = 0.5
+MODEL_SAVE_PATH = "./model/enc_dec.bin"
+
+
+class EncoderRNN(nn.Module):
+    def __init__(self, input_size: int, hidden_size: int):
+        super(EncoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+
+    def forward(self, input, hidden):
+        embedded = self.embedding(input).view(1, 1, -1)
+        output = embedded
+        output, hidden = self.gru(output, hidden)
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+
+
+class DecoderRNN(nn.Module):
+    def __init__(self, hidden_size: int, output_size: int):
+        super(DecoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+        self.out = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        output = self.embedding(input).view(1, 1, -1)
+        output = F.relu(output)
+        output, hidden = self.gru(output, hidden)
+        output = self.softmax(self.out(output[0]))
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+
+
+class AttnDecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size, dropout_p=0.1, max_length=MAX_LENGTH):
+        super(AttnDecoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.dropout_p = dropout_p
+        self.max_length = max_length
+
+        self.embedding = nn.Embedding(self.output_size, self.hidden_size)
+        self.attn = nn.Linear(self.hidden_size * 2, self.max_length)
+        self.attn_combine = nn.Linear(self.hidden_size * 2, self.hidden_size)
+        self.dropout = nn.Dropout(self.dropout_p)
+        self.gru = nn.GRU(self.hidden_size, self.hidden_size)
+        self.out = nn.Linear(self.hidden_size, self.output_size)
+
+    def forward(self, input, hidden, encoder_outputs):
+        embedded = self.embedding(input).view(1, 1, -1)
+        embedded = self.dropout(embedded)
+
+        attn_weights = F.softmax(
+            self.attn(torch.cat((embedded[0], hidden[0]), 1)), dim=1)
+        attn_applied = torch.bmm(attn_weights.unsqueeze(0),
+                                 encoder_outputs.unsqueeze(0))
+
+        output = torch.cat((embedded[0], attn_applied[0]), 1)
+        output = self.attn_combine(output).unsqueeze(0)
+
+        output = F.relu(output)
+        output, hidden = self.gru(output, hidden)
+
+        output = F.log_softmax(self.out(output[0]), dim=1)
+        return output, hidden, attn_weights
+
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+
+
+def prepare_data(df: pd.DataFrame) -> Lang:
+    lang = Lang()
+    columns = ['context', 'question', 'generated_qn']
+    for col in columns:
+        for rec in df[col].to_list():
+            lang.addSentence(rec)
+    return lang
+
+
+def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion, max_length=MAX_LENGTH):
+    encoder_hidden = encoder.initHidden()
+
+    encoder_optimizer.zero_grad()
+    decoder_optimizer.zero_grad()
+
+    input_length = input_tensor.size(0)
+    target_length = target_tensor.size(0)
+
+    encoder_outputs = torch.zeros(
+        max_length, encoder.hidden_size, device=device)
+
+    loss = 0
+
+    for ei in range(min(input_length, 50)):
+        encoder_output, encoder_hidden = encoder(
+            input_tensor[ei], encoder_hidden)
+        encoder_outputs[ei] = encoder_output[0, 0]
+
+    decoder_input = torch.tensor([[SOS_token]], device=device)
+
+    decoder_hidden = encoder_hidden
+
+    use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
+
+    if use_teacher_forcing:
+        # Teacher forcing: Feed the target as the next input
+        for di in range(target_length):
+            decoder_output, decoder_hidden, decoder_attention = decoder(
+                decoder_input, decoder_hidden, encoder_outputs)
+            loss += criterion(decoder_output, target_tensor[di])
+            decoder_input = target_tensor[di]  # Teacher forcing
+
+    else:
+        # Without teacher forcing: use its own predictions as the next input
+        for di in range(target_length):
+            decoder_output, decoder_hidden, decoder_attention = decoder(
+                decoder_input, decoder_hidden, encoder_outputs)
+            topv, topi = decoder_output.topk(1)
+            decoder_input = topi.squeeze().detach()  # detach from history as input
+
+            loss += criterion(decoder_output, target_tensor[di])
+            if decoder_input.item() == EOS_token:
+                break
+
+    loss.backward()
+
+    encoder_optimizer.step()
+    decoder_optimizer.step()
+
+    return loss.item() / target_length
+
+
+def trainIters(encoder, decoder, n_iters, df, lang, print_every=1000, plot_every=100, learning_rate=0.01):
+    start = time.time()
+    plot_losses = []
+    print_loss_total = 0  # Reset every print_every
+    plot_loss_total = 0  # Reset every plot_every
+
+    encoder_optimizer = optim.SGD(encoder.parameters(), lr=learning_rate)
+    decoder_optimizer = optim.SGD(decoder.parameters(), lr=learning_rate)
+    training_pairs = [tensorsFromPair(lang, df.iloc[random.randint(0, len(df)-1), :])
+                      for _ in range(n_iters)]
+    criterion = nn.NLLLoss()
+
+    for iter in tqdm(range(1, n_iters + 1)):
+        training_pair = training_pairs[iter - 1]
+        input_tensor = training_pair[0]
+        target_tensor = training_pair[1]
+
+        loss = train(input_tensor, target_tensor, encoder,
+                     decoder, encoder_optimizer, decoder_optimizer, criterion)
+        print_loss_total += loss
+        plot_loss_total += loss
+
+        if iter % print_every == 0:
+            print_loss_avg = print_loss_total / print_every
+            print_loss_total = 0
+            print('%s (%d %d%%) %.4f' % (timeSince(start, iter / n_iters),
+                                         iter, iter / n_iters * 100, print_loss_avg))
+
+        if iter % plot_every == 0:
+            plot_loss_avg = plot_loss_total / plot_every
+            plot_losses.append(plot_loss_avg)
+            plot_loss_total = 0
+    torch.save({
+        'encoder': encoder.state_dict(),
+        'decoder': decoder.state_dict(),
+    }, MODEL_SAVE_PATH)
+
+
+if __name__ == "__main__":
+    df: pd.DataFrame = read_dataset(DATASET_PATH)
+    print(f'device: {device}')
+    lang = prepare_data(df)
+    hidden_size = 256
+    encoder1 = EncoderRNN(lang.n_words, hidden_size).to(device)
+    attn_decoder1 = AttnDecoderRNN(
+        hidden_size, lang.n_words, dropout_p=0.1).to(device)
+    trainIters(encoder1, attn_decoder1, 75000, df, lang, print_every=5000)
---
+++ Encoder_Decoder/utils.py
@@ -0,0 +1,91 @@
+import time
+import math
+import torch
+import unicodedata
+import pandas as pd
+from collections import defaultdict
+from nltk.tokenize import word_tokenize
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def read_dataset(path: str) -> pd.DataFrame:
+    df = pd.read_csv(path)
+    print(f'Number of records in the dataset: {len(df)}')
+    print(f'Checking for NaN values: ', df.isna().sum())
+    # Remove NaN values
+    df = df.dropna()
+    print(
+        f'Number of records in the dataset after removing NaN values: {len(df)}')
+    return df
+
+
+# SOS_token = "<SOS>"
+# EOS_token = "<EOS>"
+SEP_token = "<SEP>"
+
+SOS_token = 0
+EOS_token = 1
+
+
+class Lang:
+    def __init__(self):
+        # self.word2index = {"<SOS>": 0, "<EOS>": 1, "<SEP>": 2}
+        self.word2index = {}
+        self.word2count = defaultdict(int)
+        # self.index2word = {0: "<SOS>", 1: "<EOS>", 2: "<SEP>"}
+        self.index2word = {0: "SOS", 1: "EOS"}
+        self.n_words = 2
+
+    def addSentence(self, sentence: str):
+        for word in word_tokenize(sentence):
+            self.addWord(word)
+
+    def addWord(self, word: str):
+        if word not in self.word2index:
+            self.word2index[word] = self.n_words
+            self.index2word[self.n_words] = word
+            self.n_words += 1
+        self.word2count[word] += 1
+
+    @staticmethod
+    def normalizeString(s: str):
+        # s = unicodeToAscii(s.lower().strip())
+        s = s.lower().strip()
+        return s
+
+
+# def unicodeToAscii(s):
+#     return ''.join(
+#         c for c in unicodedata.normalize('NFD', s)
+#         if unicodedata.category(c) != 'Mn'
+#     )
+def indexesFromSentence(lang, sentence):
+    return [lang.word2index[word] for word in word_tokenize(sentence)]
+
+
+def tensorFromSentence(lang, sentence):
+    indexes = indexesFromSentence(lang, sentence)
+    # indexes.append(lang.word2index[EOS_token])
+    indexes.append(EOS_token)
+    return torch.tensor(indexes, dtype=torch.long, device=device).view(-1, 1)
+
+
+def tensorsFromPair(lang, row):
+    input_tensor = tensorFromSentence(lang, row["generated_qn"])
+    target_tensor = tensorFromSentence(lang, row["question"])
+    return (input_tensor, target_tensor)
+
+
+def asMinutes(s):
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+
+def timeSince(since, percent):
+    now = time.time()
+    s = now - since
+    es = s / (percent)
+    rs = es - s
+    return '%s (- %s)' % (asMinutes(s), asMinutes(rs))
